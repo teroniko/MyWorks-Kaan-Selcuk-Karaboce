f6168733b1f9587bc46ccef47f37f25b4e38c0bb
teroniko <teroniko98@gmail.com>
2022-04-05 19:33:59 +0300

    Master Engineer Scripts

---
+++ Scripts/Buttons/Canvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Canvas : EventTrigger
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        transform.parent.gameObject.GetComponent<Buttons>().OnPointerDown(eventData);
+        Debug.Log("mjfgkhld");
+    }
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        transform.parent.gameObject.GetComponent<Buttons>().OnPointerUp(eventData);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
