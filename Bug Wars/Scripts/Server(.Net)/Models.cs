5b1f8dffb73712e5076006c3d59b6e7f5a94f202
teroniko <teroniko98@gmail.com>
2022-04-07 17:58:47 +0300

    Bug Wars (Missing)

---
+++ Bug Wars/Scripts/Server(.Net)/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tags
+{
+    public class Models
+    {
+        public enum Tags0
+        {
+            NewPlayer = 1,
+            PosRot = 2,
+            Rot = 3,
+            AttackDefenceLegdeath = 4
+
+        }
+    }
+
+}
