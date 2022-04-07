d3266330133a13ba5f62a6b7d675fcad63b36e28
teroniko <teroniko98@gmail.com>
2022-04-07 16:54:05 +0300

    Update Ball.cs

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -2,69 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
-    public Thrower thw;
-    float firsty;
-    
-    void Start()
-    {
-        firsty = transform.position.y;
-    }
-    private void Update()
-    {
-        if (firsty - 0.1f > transform.position.y)
+public class ball : MonoBehaviour {
+    public List<ground> gl = new List<ground>();
+    public Rigidbody2D r;
+
+    void Start () {
+        for (byte i = 0; i < 5; i++)
         {
-            GameObject[] Soldiers = GameObject.FindGameObjectsWithTag("Soldier");
-            //RaycastHit hit;
-            //if (other.tag == "Soldier" &&
-            //    Physics.Raycast(transform.position, other.transform.position - transform.position, out hit, 2))
-            //{
-            //    float distance = Vector3.Distance(transform.position, other.transform.position - transform.position);
-            //    if (distance >= 2.0f)
-            //    {
-            //        thw.GetComponent<Target>().Hit(1);
-            //        //zaten collider var raycast ekleme
-            //        Debug.Log("hit.distance : " + hit.distance);
-            //    }
-            //}
-            foreach (GameObject Soldier in Soldiers)
-            {
-                RaycastHit hit;
-                float explodeDistance = 1.5f;
-                //LayerMask mask = LayerMask.GetMask("Soldier");
-                if (Physics.Raycast(transform.position, Soldier.transform.position-transform.position, out hit, explodeDistance)
-                   && Soldier.transform.parent.gameObject == hit.transform.parent.gameObject)
-                //    int layerMask = 1 << 10;
-                //if (Physics.Raycast(transform.position,Soldier.transform.position - transform.position,out hit, 3, layerMask))
-                {
-                    //geri itme yap
-                    //atış hızı * soldier hızı bölgesine at
-                    //collider engelliyor değil
-                    Debug.Log("Ball");
-                    Soldier.GetComponent<Target>().HitThis(hit.distance * 1.0f/*3.0f*/, thw.GetComponent<Target>().self.attackPower);
-                }
-            }
-            gameObject.SetActive(false);
-
-
-
+            gl.Add(new ground(i, 5 - 2.5f * (i - 1) - 1));
         }
     }
-    //private void OnTriggerStay(Collider other)
-    //{
-        //RaycastHit hit;
-        //if(other.tag=="Soldier"&&
-        //    Physics.Raycast(transform.position, other.transform.position - transform.position, out hit, 2))
-        //{
-        //    float distance = Vector3.Distance(transform.position, other.transform.position - transform.position);
-        //    if (distance >= 2.0f)
-        //    {
-        //        thw.GetComponent<Target>().Hit(1);
-        //        //zaten collider var raycast ekleme
-        //        Debug.Log("hit.distance : "+hit.distance);
-        //    }
-        //}
-        //gameObject.SetActive(false);
-    //}
+	
+	
+	void Update () {
+		
+	}
 }
